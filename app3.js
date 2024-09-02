700a93a914523671b4d20aef546ca5450e84561d
Amir <amiraezm@gmail.com>
2024-09-02 19:21:12 +0330

    first commit

---
+++ app3.js
@@ -0,0 +1,11 @@
+const kyle = require('./names');
+const sayHooy = require('./utils');
+const data = require('./alternative-flavor')
+require('./mind-grenade')
+
+console.log(data);
+
+
+sayHooy('susan')
+sayHooy(kyle.john)
+sayHooy(kyle.peter)
